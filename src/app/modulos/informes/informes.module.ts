57169df2355ae440d5c3abf81bef75e5e9f00d8b
Julian Florez <julian.1701913963@ucaldas.edu.co>
2021-04-18 17:07:52 -0500

    Creacion de los modulos

---
+++ src/app/modulos/informes/informes.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InformesRoutingModule } from './informes-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    InformesRoutingModule
+  ]
+})
+export class InformesModule { }
---
+++ src/app/modulos/soliciud-estudio/soliciud-estudio.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SoliciudEstudioRoutingModule } from './soliciud-estudio-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, SoliciudEstudioRoutingModule],
+})
+export class SoliciudEsudioModule {}
