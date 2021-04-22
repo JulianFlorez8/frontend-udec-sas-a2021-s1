c936dcf59edb7536eba3ba9baaf23109f44eb1aa
Julian Florez <julian.1701913963@ucaldas.edu.co>
2021-04-21 23:32:49 -0500

    Creacion del side nav y de la tabla usuarios

--- src/assets/js/materializeInit.js
+++ src/assets/js/materializeInit.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".sidenav");
-  var instances = M.Sidenav.init(elems, {});
+  var instances = M.Sidenav.init(elems);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".dropdown-trigger");
+  var instances = M.Dropdown.init(elems);
 });
