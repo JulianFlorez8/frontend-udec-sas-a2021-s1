2e49ca5c4b1df41f6acd37c6de76d24df52fa2e4
Julian Florez <julian.1701913963@ucaldas.edu.co>
2021-04-27 15:46:06 -0500

    Tablas proyectos,bloques e inmuebles listan correctamente

--- src/app/modulos/parametrizacion/bloque/listar-bloque/listar-bloque.component.ts
+++ src/app/modulos/parametrizacion/bloque/listar-bloque/listar-bloque.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { BloqueModel } from 'src/app/models/parametrizacion/bloque.model';
+import { BloqueService } from '../../../../services/parametrizacion/bloque.service';
 
 @Component({
   selector: 'app-listar-bloque',
   templateUrl: './listar-bloque.component.html',
-  styleUrls: ['./listar-bloque.component.css']
+  styleUrls: ['./listar-bloque.component.css'],
 })
 export class ListarBloqueComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private service: BloqueService) {}
+  lista: BloqueModel[] = [];
   ngOnInit(): void {
+    this.obtenerLista();
   }
 
+  obtenerLista() {
+    this.service.obtenerBloques().subscribe(
+      (datos) => {
+        this.lista = datos;
+        console.log(this.lista);
+      },
+      (error) => {
+        console.log('error');
+      }
+    );
+  }
 }
