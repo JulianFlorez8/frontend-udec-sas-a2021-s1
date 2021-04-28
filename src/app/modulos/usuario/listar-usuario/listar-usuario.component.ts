a4779dd47db7033719d23c527dd7778b372e5f26
Julian Florez <julian.1701913963@ucaldas.edu.co>
2021-04-27 19:45:02 -0500

    Botón crear usuario solo para admin

--- src/app/modulos/usuario/listar-usuario/listar-usuario.component.ts
+++ src/app/modulos/usuario/listar-usuario/listar-usuario.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioModel } from 'src/app/models/usuario.model';
 import { UsuariosService } from '../../../services/usuarios.service';
+import { Subscription } from 'rxjs';
+import { SeguridadService } from 'src/app/services/seguridad/seguridad.service';
+
 @Component({
   selector: 'app-listar-usuario',
   templateUrl: './listar-usuario.component.html',
   styleUrls: ['./listar-usuario.component.css'],
 })
 export class ListarUsuarioComponent implements OnInit {
-  constructor(private service: UsuariosService) {}
+  constructor(
+    private service: UsuariosService,
+    private secService: SeguridadService
+  ) {}
   lista: UsuarioModel[] = [];
+
+  suscripcion?: Subscription;
+  rol: string | undefined = '';
   ngOnInit(): void {
     this.obtenerLista();
+    this.suscripcion = this.secService.getDatosUsuario().subscribe((data) => {
+      this.rol = data.usuario?.Rol;
+    });
   }
 
   obtenerLista() {
