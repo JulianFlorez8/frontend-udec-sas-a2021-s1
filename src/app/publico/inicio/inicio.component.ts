ac30f9a66cf445e908d919da8b43388103ff5c67
Julian Florez <julian.1701913963@ucaldas.edu.co>
2021-06-30 14:54:05 -0500

    color

--- src/app/publico/inicio/inicio.component.ts
+++ src/app/publico/inicio/inicio.component.ts
@@ -12,13 +12,14 @@ import { ProyectoService } from 'src/app/services/parametrizacion/proyecto.servi
   styleUrls: ['./inicio.component.css'],
 })
 export class InicioComponent implements OnInit {
-  constructor(private service: ProyectoService,
+  constructor(
+    private service: ProyectoService,
     private servicioCiudad: CiudadService,
     private servicioPais: PaisService,
     private servicioBloque: BloqueService,
     private servicioDescarga: DescargaArchivosService
-    ) {}
-  lista:  ProyectoModel[]=[];
+  ) {}
+  lista: ProyectoModel[] = [];
   ngOnInit(): void {
     this.obtenerLista();
   }
@@ -26,38 +27,58 @@ export class InicioComponent implements OnInit {
   obtenerLista() {
     this.service.obtenerProyectos().subscribe(
       (datos) => {
-        let codigoPais=0;
-        
-        datos.forEach(dato=>{
-          let contador=0;
-          if(dato.codigoCiudad)
-          this.servicioCiudad.obtenerCiudad(dato.codigoCiudad).subscribe(
-            (ciudad)=>{
-              dato.ciudad=ciudad.nombre; 
-              codigoPais=ciudad.codigoPais;
-              if(codigoPais>0)
-              this.servicioPais.obtenerPais(codigoPais).subscribe((pais)=>{dato.pais=pais.nombre});
+        let codigoPais = 0;
+
+        datos.forEach((dato) => {
+          let contador = 0;
+          if (dato.codigoCiudad)
+            this.servicioCiudad
+              .obtenerCiudad(dato.codigoCiudad)
+              .subscribe((ciudad) => {
+                dato.ciudad = ciudad.nombre;
+                codigoPais = ciudad.codigoPais;
+                if (codigoPais > 0)
+                  this.servicioPais
+                    .obtenerPais(codigoPais)
+                    .subscribe((pais) => {
+                      dato.pais = pais.nombre;
+                    });
+              });
+          if (dato.codigo)
+            this.service
+              .obtenerBloquesProyecto(dato.codigo)
+              .subscribe((bloques) => {
+                bloques.forEach((bloque) => {
+                  if (bloque.codigo)
+                    this.servicioBloque
+                      .obtenerBloqueInmueble(bloque.codigo)
+                      .subscribe((inmuebles) => {
+                        contador = inmuebles.length + contador;
+                        dato.inmueblesDisponibles = contador;
+                      });
+                });
+              });
+          this.servicioDescarga
+            .descargarArchivo(1, dato.imagen)
+            .subscribe((imagen) => {
+              dato.imagen = imagen.ruta;
             });
-          if(dato.codigo)
-          this.service.obtenerBloquesProyecto(dato.codigo).subscribe((bloques)=>{
-            bloques.forEach(bloque=>{
-              if(bloque.codigo)
-              this.servicioBloque.obtenerBloqueInmueble(bloque.codigo).subscribe(inmuebles=>{
-                contador=inmuebles.length+contador;
-                dato.inmueblesDisponibles=contador;
-              })
-            });
-          });
-          this.servicioDescarga.descargarArchivo(1,dato.imagen).subscribe((imagen)=>{
-            dato.imagen=imagen.ruta;
-          })
           this.lista.push(dato);
-        })
+        });
         console.log(this.lista);
       },
       (error) => {
         console.log('error');
       }
     );
+  }
+
+  btnClick(s: any): void {
+    let x = document.querySelector(`#card${s}`) as HTMLElement;
+    if (x.style.display === 'none') {
+      x.style.display = 'block';
+    } else {
+      x.style.display = 'none';
+    }
   }
 }
